c3e68cf1a1022974525970d175a1bca4db3fe62a
Asad ullah <theasadse@gmail.com>
2025-12-28 14:33:03 +0000

    fixed schema of admin notification

--- src/graphql/schema/notification.ts
+++ src/graphql/schema/notification.ts
@@ -1,37 +1,39 @@
-import { gql } from 'apollo-server';
+import { gql } from "apollo-server";
 
 const notification = gql`
-type Notification {
-  id: ID!
-  name: String!
-  message: String
-  isRead: Boolean!
-  createdAt: DateTime!
-  user: User!
-}
+  type Notification {
+    id: ID!
+    name: String!
+    message: String
+    isRead: Boolean!
+    createdAt: DateTime!
+    user: User!
+  }
 
-type NotificationReturn {
-  notifications: [Notification!]!
-  total:Int
-}
+  type NotificationReturn {
+    notifications: [Notification!]!
+    total: Int
+  }
 
-input NotificationFilter {
-  search:String
-  isRead:Boolean
-  startDate: DateTime
-  endDate:DateTime
-}
-type Query {
-  getNotifications(userId: ID!): NotificationReturn!
-  getNotification(id: ID!): Notification
-  getAdminNotifications(filter: NotificationFilter): NotificationReturn!
-}
+  input NotificationFilter {
+    search: String
+    isRead: Boolean
+    startDate: DateTime
+    endDate: DateTime
+    page: Int
+    limit: Int
+  }
+  type Query {
+    getNotifications(userId: ID!): NotificationReturn!
+    getNotification(id: ID!): Notification
+    getAdminNotifications(filter: NotificationFilter): NotificationReturn!
+  }
 
-type Mutation {
-  # createNotification(userId: ID!, title: String!, message: String!): Notification!
-  markNotificationAsRead(id: ID!): Notification!
-  markAllNotificationsASRead(userId:ID):Boolean!
-}
-  `;
+  type Mutation {
+    # createNotification(userId: ID!, title: String!, message: String!): Notification!
+    markNotificationAsRead(id: ID!): Notification!
+    markAllNotificationsASRead(userId: ID): Boolean!
+  }
+`;
 
 export default notification;
