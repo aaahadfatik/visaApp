1e887f38ec19a36f9237956f37866e55f91b1347
theasadse <theasadse@gmail.com>
2025-12-31 22:27:38 +0500

    fixed the search from getUsers

--- src/graphql/resolvers/userResolver.ts
+++ src/graphql/resolvers/userResolver.ts
@@ -1,11 +1,17 @@
-import { Role, User,Document, FormSubmission } from '../../entity';
-import { dataSource } from '../../datasource';
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
-import { authenticate } from '../../utils/authUtils';
-import { CreateRoleInput, CreateUserInput, UpdateRoleInput, UpdateUserInput,UserFilter } from '../../types';
-import { FormStatus } from '../../enum';
-import { Between } from 'typeorm';
+import { Role, User, Document, FormSubmission } from "../../entity";
+import { dataSource } from "../../datasource";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { authenticate } from "../../utils/authUtils";
+import {
+  CreateRoleInput,
+  CreateUserInput,
+  UpdateRoleInput,
+  UpdateUserInput,
+  UserFilter,
+} from "../../types";
+import { FormStatus } from "../../enum";
+import { Between, ILike } from "typeorm";
 
 const roleRepository = dataSource.getRepository(Role);
 const userRepository = dataSource.getRepository(User);
@@ -14,21 +20,29 @@ const submittedFomrRepository = dataSource.getRepository(FormSubmission);
 
 const userResolvers = {
   Query: {
-    getRoles: async(_:any)=>{
-        const userRoles = await roleRepository.find();
-        return userRoles
+    getRoles: async (_: any) => {
+      const userRoles = await roleRepository.find();
+      return userRoles;
     },
-    getUser: async (_: any, { id }: { id: string },context: any) => {
-        // const authUser = await authenticate(context);
-        // if (!authUser) {
-        //     throw new ApolloError('Unauthorized access', 'UNAUTHORIZED');
-        // }
-        return await userRepository.findOne({
-          where: { id },
-          relations: ['documents', 'notifications', 'applications'],
-        });
+    getUser: async (_: any, { id }: { id: string }, context: any) => {
+      // const authUser = await authenticate(context);
+      // if (!authUser) {
+      //     throw new ApolloError('Unauthorized access', 'UNAUTHORIZED');
+      // }
+      return await userRepository.findOne({
+        where: { id },
+        relations: ["documents", "notifications", "applications"],
+      });
     },
-    getUsers: async (_: any, { limit, offset,filter }: { limit: number; offset: number,filter:UserFilter },context: any) => {
+    getUsers: async (
+      _: any,
+      {
+        limit,
+        offset,
+        filter,
+      }: { limit: number; offset: number; filter: UserFilter },
+      context: any
+    ) => {
       let whereClause: any = {};
       if (filter) {
         if (filter.status !== undefined) {
@@ -38,41 +52,47 @@ const userResolvers = {
           whereClause.isCompany = filter.type;
         }
         if (filter.search) {
-          whereClause.name =  dataSource.driver.escape(`%${filter.search}%`);
+          whereClause.name = ILike(`%${filter.search}%`);
         }
       }
-      const [users,total] = await userRepository.findAndCount({
+      const [users, total] = await userRepository.findAndCount({
         where: whereClause,
-        take: limit ,
+        take: limit,
         skip: offset,
-        order:{createdAt:'DESC'},
-        relations: ['documents', 'notifications', 'applications'],
+        order: { createdAt: "DESC" },
+        relations: ["documents", "notifications", "applications"],
       });
-      if (!users) throw new Error('Users not found');
+      if (!users) throw new Error("Users not found");
       const usersWithCounts = await Promise.all(
         users.map(async (user) => {
           const submittedFromCount = await submittedFomrRepository.count({
             where: { createdBy: user.id },
           });
-    
+
           return {
             ...user,
             submittedFromCount,
           };
         })
       );
-    
-      return {users:usersWithCounts,total};
+
+      return { users: usersWithCounts, total };
     },
     getUserTypesCount: async (_: any, __: any, context: any) => {
-      const companyCount = await userRepository.count({ where: { isCompany: true } });
-      const individualCount = await userRepository.count({ where: { isCompany: false } });
+      const companyCount = await userRepository.count({
+        where: { isCompany: true },
+      });
+      const individualCount = await userRepository.count({
+        where: { isCompany: false },
+      });
       return { companyCount, individualCount };
     },
-    getDashboardStatistics: async (_: any, __: any,) => {
+    getDashboardStatistics: async (_: any, __: any) => {
       const totalUsers = await userRepository.count();
       const applicationsSubmitted = await submittedFomrRepository.count();
-      const pendingApplications = await submittedFomrRepository.count({ where: { status: FormStatus.UNDER_PROGRESS } });
+      const pendingApplications = await submittedFomrRepository.count({
+        where: { status: FormStatus.UNDER_PROGRESS },
+      });
       const today = new Date();
       today.setHours(0, 0, 0, 0);
       const tomorrow = new Date(today);
@@ -82,10 +102,14 @@ const userResolvers = {
           createdAt: Between(today, tomorrow),
         },
       });
-      return { totalUsers, applicationsSubmitted, pendingApplications, todayApplications };
+      return {
+        totalUsers,
+        applicationsSubmitted,
+        pendingApplications,
+        todayApplications,
+      };
     },
-    getRegisteredUsersGraph: async ( _: any, { year,}: { year?: string }
-    ) => {
+    getRegisteredUsersGraph: async (_: any, { year }: { year?: string }) => {
       const userRepo = dataSource.getRepository(User);
       const currentYear = year ? parseInt(year, 10) : new Date().getFullYear();
       const data: { companyCount: number; individualCount: number }[] = [];
@@ -106,61 +130,61 @@ const userResolvers = {
         });
         data.push({ companyCount, individualCount });
       }
-      return { year: currentYear.toString(), data}
-    },    
+      return { year: currentYear.toString(), data };
+    },
   },
   Mutation: {
-    createRole:  async (_:any,{input}:{input:CreateRoleInput})=>{
-      try{
+    createRole: async (_: any, { input }: { input: CreateRoleInput }) => {
+      try {
         const newRole = roleRepository.create({
           ...input,
-        })
+        });
         await roleRepository.save(newRole);
-        return newRole
-      }catch(error){
-        throw new Error('Error on creating role');
+        return newRole;
+      } catch (error) {
+        throw new Error("Error on creating role");
       }
     },
-    updateRole:  async (_:any,{input}:{input:UpdateRoleInput})=>{
-      try{
+    updateRole: async (_: any, { input }: { input: UpdateRoleInput }) => {
+      try {
         const role = await roleRepository.findOne({ where: { id: input.id } });
-        if (!role) throw new Error('Role not found');
+        if (!role) throw new Error("Role not found");
 
         Object.assign(role, input);
         await roleRepository.save(role);
         return role;
-      }catch(error){
-        throw new Error('Error on creating role');
+      } catch (error) {
+        throw new Error("Error on creating role");
       }
     },
-    login: async (_: any, input: {email:string, password: string }) => {
+    login: async (_: any, input: { email: string; password: string }) => {
       const { password, email } = input;
-    
+
       const user = await userRepository.findOne({
         where: { email },
-        relations:['role']
+        relations: ["role"],
       });
-    
-      if (!user) throw new Error('User not found');
-      if (!password) throw new Error('Password is required');
+
+      if (!user) throw new Error("User not found");
+      if (!password) throw new Error("Password is required");
 
       // Validate password
       const validPassword = await bcrypt.compare(password, user.password);
-    
-      if (!validPassword)  throw new Error('Invalid password');
-    
+
+      if (!validPassword) throw new Error("Invalid password");
+
       // Generate JWT tokens
       const token = jwt.sign(
         { userId: user.id, email: user.email },
         process.env.JWT_SECRET!,
-        { expiresIn: '30d' }
+        { expiresIn: "30d" }
       );
       const refreshToken = jwt.sign(
         { userId: user.id, email: user.email },
         process.env.JWT_REFRESH_SECRET!,
-        { expiresIn: '30d' }
+        { expiresIn: "30d" }
       );
-    
+
       // Update the user with the new refresh token
       user.refreshToken = token; // Ensure refreshToken is defined in User entity
       await userRepository.save(user);
@@ -173,138 +197,166 @@ const userResolvers = {
           name: user.name,
           email: user.email,
           roles: Role.name,
-          ClockedInAt: new Date()
+          ClockedInAt: new Date(),
         },
       };
     },
     logout: async (_: any, __: any, context: any) => {
       try {
         const decoded = authenticate(context);
-        if (!decoded) throw new Error('Unauthorized');
+        if (!decoded) throw new Error("Unauthorized");
 
-        const user = await userRepository.findOne({ where: { id: (await decoded).userId } });
-        if (!user) throw new Error('User not found');
+        const user = await userRepository.findOne({
+          where: { id: (await decoded).userId },
+        });
+        if (!user) throw new Error("User not found");
 
         user.refreshToken = "";
         await userRepository.save(user);
 
         return {
-          message: "Logout successful"
+          message: "Logout successful",
         };
-    
-      } catch (error:any) {
-        console.error('Logout error:', error);
+      } catch (error: any) {
+        console.error("Logout error:", error);
         throw new Error(error.message);
       }
-    },      
-    createUser:  async (_: any, { input }: { input: CreateUserInput }, context: any) => {
-      try{
-          // const usercxt = await authenticate(context);
-          // const userRole = await roleRepository.findOne({ where: { id: input.roleId } });
-          const saltRounds = 10;
+    },
+    createUser: async (
+      _: any,
+      { input }: { input: CreateUserInput },
+      context: any
+    ) => {
+      try {
+        // const usercxt = await authenticate(context);
+        // const userRole = await roleRepository.findOne({ where: { id: input.roleId } });
+        const saltRounds = 10;
 
-          // Check if the user already exists
-          const existingUser = await userRepository.findOne({ where: { email: input.email } });
-          if (existingUser) throw new Error('User already exists');
+        // Check if the user already exists
+        const existingUser = await userRepository.findOne({
+          where: { email: input.email },
+        });
+        if (existingUser) throw new Error("User already exists");
 
-          // if (!userRole) {
-          //   throw new Error('Specified role does not exist');
-          // }
+        // if (!userRole) {
+        //   throw new Error('Specified role does not exist');
+        // }
 
-          // Hash the password
-          const hashedPassword = await bcrypt.hash(input.password, saltRounds);
-          const { password, ...userData } = input;
-          if (input.picture) input.picture 
+        // Hash the password
+        const hashedPassword = await bcrypt.hash(input.password, saltRounds);
+        const { password, ...userData } = input;
+        if (input.picture) input.picture;
 
-          // Create the new user
-          const newUser = userRepository.create({
-            ...userData,
-            picture: input.picture,
-            // role: userRole,
-            password: hashedPassword,
-            createdAt:new Date()
-          });
+        // Create the new user
+        const newUser = userRepository.create({
+          ...userData,
+          picture: input.picture,
+          // role: userRole,
+          password: hashedPassword,
+          createdAt: new Date(),
+        });
 
+        // Save the user in the database
+        const savedUser = await userRepository.save(newUser);
 
-          // Save the user in the database
-          const savedUser = await userRepository.save(newUser);
-
-          // Process vehicle pictures array
-          if (input.documents && input.documents.length > 0) {
-            await Promise.all(input.documents.map(async (pic) => {
+        // Process vehicle pictures array
+        if (input.documents && input.documents.length > 0) {
+          await Promise.all(
+            input.documents.map(async (pic) => {
               const newDoc = documentRepository.create({
                 ...pic,
                 user: savedUser, // ✅ connect to the user properly
               });
               await documentRepository.save(newDoc);
-            }));
-          }
+            })
+          );
+        }
 
-          // Return the newly created user (excluding the password)
-          return {
-           ...newUser,
+        // Return the newly created user (excluding the password)
+        return {
+          ...newUser,
         };
-      }catch(error){
-        throw Error(`Error on create User${error}`)
+      } catch (error) {
+        throw Error(`Error on create User${error}`);
       }
     },
-    updateUser:  async (_: any, { input }: { input: UpdateUserInput }, context: any) => {
-      try{
+    updateUser: async (
+      _: any,
+      { input }: { input: UpdateUserInput },
+      context: any
+    ) => {
+      try {
         // Fetch the existing user
-        const existingUser = await userRepository.findOne({ where: { id: input.id } });
-        if (!existingUser) throw new Error('User not found');
-      
+        const existingUser = await userRepository.findOne({
+          where: { id: input.id },
+        });
+        if (!existingUser) throw new Error("User not found");
+
         // Check if the email is being updated and is unique
         if (input.email && input.email !== existingUser.email) {
-          const userWithEmail = await userRepository.findOne({ where: { email: input.email } });
-          if (userWithEmail) throw new Error('Email already in use by another user');
+          const userWithEmail = await userRepository.findOne({
+            where: { email: input.email },
+          });
+          if (userWithEmail)
+            throw new Error("Email already in use by another user");
         }
-      
+
         // Fetch the new role if provided
         let userRole = undefined;
         if (input.roleId) {
-          userRole = await roleRepository.findOne({ where: { id: input.roleId } });
+          userRole = await roleRepository.findOne({
+            where: { id: input.roleId },
+          });
           if (!userRole) {
-            throw new Error('Specified role does not exist');
+            throw new Error("Specified role does not exist");
           }
         }
-      
+
         // Update the user data, excluding the password if not updated
         const updatedUser = {
           ...existingUser,
           ...input,
           role: userRole || existingUser.role, // Only update role if new one is provided
         };
-      
+
         // Save the updated user in the database
         await userRepository.save(updatedUser);
-      
+
         // Return the updated user (excluding the password)
         return {
           ...updatedUser,
           password: undefined, // Exclude password from the returned object
         };
-      }catch(error){
-        throw Error(`error on updating User ${error}`)
+      } catch (error) {
+        throw Error(`error on updating User ${error}`);
       }
     },
     deleteUser: async (_: any, { id }: { id: string }, context: any) => {
       const user = await userRepository.findOne({ where: { id } });
       if (!user) {
-        throw new Error('User not found');
+        throw new Error("User not found");
       }
       user.isDeleted = true;
       await userRepository.save(user);
       return true;
     },
-    changePassword: async (_: any, { oldPassword, newPassword }: { oldPassword: string; newPassword: string }, context: any) => {
+    changePassword: async (
+      _: any,
+      {
+        oldPassword,
+        newPassword,
+      }: { oldPassword: string; newPassword: string },
+      context: any
+    ) => {
       const authUser = await authenticate(context);
-      const user = await userRepository.findOne({ where: { id: authUser.userId } });
-      if (!user) throw new Error('User not found');
+      const user = await userRepository.findOne({
+        where: { id: authUser.userId },
+      });
+      if (!user) throw new Error("User not found");
 
       // Validate old password
       const validPassword = await bcrypt.compare(oldPassword, user.password);
-      if (!validPassword) throw new Error('Old password is incorrect');
+      if (!validPassword) throw new Error("Old password is incorrect");
 
       // Hash new password
       const saltRounds = 10;
@@ -317,20 +369,30 @@ const userResolvers = {
       return true;
     },
     refreshToken: async (_: any, { token }: { token: string }) => {
-      if (!token) throw new Error('No token provided');
+      if (!token) throw new Error("No token provided");
       try {
         // Verify refresh token
-        const decoded: any = jwt.verify(token, process.env.REFRESH_TOKEN_SECRET!);
-        const user = await userRepository.findOne({ where: { id: decoded.userId } });
-    
-        if (!user || user.refreshToken !== token)  throw new Error('Invalid refresh token');
-    
+        const decoded: any = jwt.verify(
+          token,
+          process.env.REFRESH_TOKEN_SECRET!
+        );
+        const user = await userRepository.findOne({
+          where: { id: decoded.userId },
+        });
+
+        if (!user || user.refreshToken !== token)
+          throw new Error("Invalid refresh token");
+
         // Generate a new access token
-        const accessToken = jwt.sign({ userId: user.id, Email: user.email }, process.env.JWT_SECRET!, { expiresIn: '10m' });
-    
+        const accessToken = jwt.sign(
+          { userId: user.id, Email: user.email },
+          process.env.JWT_SECRET!,
+          { expiresIn: "10m" }
+        );
+
         return { accessToken };
       } catch (error) {
-        throw new Error('Invalid or expired refresh token');
+        throw new Error("Invalid or expired refresh token");
       }
     },
   },
