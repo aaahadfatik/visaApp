28a95f1d616bbb7d4a495dac32983bb583eacc7d
aaafatik <abdulahadfatik4@gmail.com>
2026-01-18 17:38:45 +0500

    fix

--- src/graphql/resolvers/serviceResolvers.ts
+++ src/graphql/resolvers/serviceResolvers.ts
@@ -352,14 +352,6 @@ const serviceResolvers = {
         status: FormStatus.UNDER_PROGRESS,
       })
       .getMany();
-
-        console.log(
-          submissions.map(s => ({
-            submissionId: s.id,
-            visa: s.visa,
-            category: s.visa?.category,
-          }))
-        );
         
         return submissions.map((s) => ({
           id: s.id,
