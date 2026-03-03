5e43c2e647c96f2aae3c8725fe61198c7afd68ba
theasadse <theasadse@gmail.com>
2026-03-03 16:37:19 +0500

    create the getadmin user

--- src/graphql/resolvers/userResolver.ts
+++ src/graphql/resolvers/userResolver.ts
@@ -44,7 +44,7 @@ const userResolvers = {
           "documents",
           "notifications",
           "applications",
-          "submittedForms",
+          "submissions",
         ],
       });
     },
