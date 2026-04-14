e232b2c8a63949f5074d02434d7c87c8fae3dc2a
theasadse <theasadse@gmail.com>
2026-04-14 23:13:17 +0500

    fixed the env.

--- src/datasource.ts
+++ src/datasource.ts
@@ -1,18 +1,50 @@
-import { DataSource } from 'typeorm';
-import { 
-  User,Role,Document,Notification,Chat,Message,Application,Service,Form,FormAttribute,Visa,Category,FormSubmission,Payment,CategoryAttribute,OTP
-} from './entity';
-import dotenv from 'dotenv';
 dotenv.config();
+import dotenv from "dotenv";
+import { DataSource } from "typeorm";
+import {
+  User,
+  Role,
+  Document,
+  Notification,
+  Chat,
+  Message,
+  Application,
+  Service,
+  Form,
+  FormAttribute,
+  Visa,
+  Category,
+  FormSubmission,
+  Payment,
+  CategoryAttribute,
+  OTP,
+} from "./entity";
 
 export const dataSource = new DataSource({
-  type: 'postgres',
+  type: "postgres",
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
   database: process.env.DB_NAME,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  entities: [ Role,User,Document,Notification,Chat,Message,Application,Service,Form,FormAttribute,Visa,Category,FormSubmission,Payment,CategoryAttribute,OTP],
+  entities: [
+    Role,
+    User,
+    Document,
+    Notification,
+    Chat,
+    Message,
+    Application,
+    Service,
+    Form,
+    FormAttribute,
+    Visa,
+    Category,
+    FormSubmission,
+    Payment,
+    CategoryAttribute,
+    OTP,
+  ],
   synchronize: true,
   logging: true,
 });
--- src/service/emailService.ts
+++ src/service/emailService.ts
@@ -1,8 +1,7 @@
-import nodemailer from "nodemailer";
-import dotenv from "dotenv";
-import { logger } from "../utils/logger";
-
 dotenv.config({ path: "../../.env" });
+import dotenv from "dotenv";
+import nodemailer from "nodemailer";
+import { logger } from "../utils/logger";
 
 interface SendEmailParams {
   to: string;
@@ -11,29 +10,28 @@ interface SendEmailParams {
 }
 
 const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: Number(process.env.EMAIL_PORT),
-    secure: false, // MUST be false for port 587
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });  
+  host: process.env.EMAIL_HOST,
+  port: Number(process.env.EMAIL_PORT),
+  secure: false, // MUST be false for port 587
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
-  export const sendEmail = async ({ to, subject, html }: SendEmailParams) => {
-    try {
-      await transporter.sendMail({
-        from: `"Alem" <${process.env.EMAIL_FROM}>`,
-        to,
-        subject,
-        html,
-      });
-  
-      logger.info(`Email sent to ${to}`);
-      return true;
-    } catch (error: any) {
-      logger.error(`Failed to send email to ${to}`, error);
-      return false;
-    }
-  };
-  
+export const sendEmail = async ({ to, subject, html }: SendEmailParams) => {
+  try {
+    await transporter.sendMail({
+      from: `"Alem" <${process.env.EMAIL_FROM}>`,
+      to,
+      subject,
+      html,
+    });
+
+    logger.info(`Email sent to ${to}`);
+    return true;
+  } catch (error: any) {
+    logger.error(`Failed to send email to ${to}`, error);
+    return false;
+  }
+};
