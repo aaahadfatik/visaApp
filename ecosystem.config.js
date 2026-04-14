ef1ad51a238ed2d5e97366efbdb9737bb899dd1a
theasadse <theasadse@gmail.com>
2026-04-15 01:15:19 +0500

    fixed issues

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,11 +3,13 @@ module.exports = {
     {
       name: "visaapp",
       script: "src/server.ts",
+      interpreter: "node",
+      interpreter_args: "-r ts-node/register",
       env: {
         DB_HOST: "localhost",
         DB_PORT: 5436,
         DB_NAME: "docUpload",
-        DB_USERNAME: "postgres",
+        DB_USER: "postgres",
         DB_PASSWORD: "password",
       },
     },
